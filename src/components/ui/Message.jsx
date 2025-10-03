d1d02244acc587767c3dec06033ad3ef777ace22
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-10-03 20:34:24 +0600

    feat: Sign in impllemented

--- src/components/ui/Message.jsx
+++ src/components/ui/Message.jsx
@@ -4,19 +4,19 @@ const Message = ({ status }) => {
     return (
         <>
             {status?.type === "success" && (
-                <p className="text-green-400 font-bold">{status.message}</p>
+                <p className="text-sm text-green-400 font-bold">{status.message}</p>
             )}
 
             {status?.type === "cors" && status.ok (
-                <p className="text-green-400 font-bold">'Email Verified'</p>
+                <p className="text-sm text-green-400 font-bold">'Email Verified'</p>
             )}
 
             {status?.type === "error" && (
-                <p className="text-red-400 font-bold">{status.message}</p>
+                <p className="text-sm text-red-400 font-bold">{status.message}</p>
             )}
 
             {status?.type === "server-error" && (
-                <p className="text-red-400 font-bold">Server error: {status.message}</p>
+                <p className="text-sm text-red-400 font-bold">Server error: {status.message}</p>
             )}
         </>
     );
