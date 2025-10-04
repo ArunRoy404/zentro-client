36dea6520c01508aa28dbf607d26c880d3e90d13
Abdul Al Roman <mdroman45678910@gmail.com>
2025-10-04 19:34:00 +0600

    feat: my first commit on dev/roman

--- src/page/AboutPage.jsx
+++ src/page/AboutPage.jsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
   return (
     <PageHeading
       title="About Zentro Realty"
-      subtitle="At Zentro Realty, we’re passionate about helping people find their dream homes. With years of experience and a deep understanding of the real estate market, we provide trusted guidance and personalized services to make buying, selling, or renting a property seamless and stress-free."
+      subtitle="At Zentro Realty, we’re passionate about helping people find their dream homes. With years of experience and a deep understanding of the real estate market, we provide trusted guidance and personalized services to make buying, selling, or renting a property seamless and stress-free. "
     ></PageHeading>
   );
 };
