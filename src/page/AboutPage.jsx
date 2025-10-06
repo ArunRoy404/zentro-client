97f6fdc020c910852dca744b7b6875212a293fe4
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-10-06 18:26:46 +0600

    feat (about) : Add About Heading

--- src/page/AboutPage.jsx
+++ src/page/AboutPage.jsx
@@ -1,12 +1,18 @@
 import PageHeading from "@/components/shared/PageHeading";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import AboutHeading from "@/section/about/AboutHeading";
 import React from "react";
 
 const AboutPage = () => {
   return (
-    <PageHeading
-      title="About Zentro Realty"
-      subtitle="At Zentro Realty, we’re passionate about helping people find their dream homes. With years of experience and a deep understanding of the real estate market, we provide trusted guidance and personalized services to make buying, selling, or renting a property seamless and stress-free."
-    ></PageHeading>
+    <div>
+      <PageHeading
+        title="About Zentro Realty"
+        subtitle="At Zentro Realty, we’re passionate about helping people find their dream homes. With years of experience and a deep understanding of the real estate market, we provide trusted guidance and personalized services to make buying, selling, or renting a property seamless and stress-free."
+      ></PageHeading>
+      <AboutHeading></AboutHeading>
+    </div>
   );
 };
 
---
+++ src/section/about/AboutHeading.jsx
@@ -0,0 +1,13 @@
+export default function AboutHeading() {
+  return (
+    <div className="text-center max-w-3xl mx-auto py-10">
+      <h1 className="text-4xl md:text-5xl font-bold mb-4">
+        <span className="text-secondary">About</span> Our Company
+      </h1>
+      <p className="text-lg text-secondary">
+        Building trust, connecting people, and creating opportunities in the
+        world of real estate.
+      </p>
+    </div>
+  );
+}
