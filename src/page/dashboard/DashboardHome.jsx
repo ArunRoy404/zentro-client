a9178f5b0a7c2eadaa810a7d2c394aebf94c84ab
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-10-07 00:09:01 +0600

    feat: Chart added in dashboard

--- src/page/dashboard/DashboardHome.jsx
+++ src/page/dashboard/DashboardHome.jsx
@@ -1,15 +1,19 @@
+import { ChartAreaInteractive } from "@/components/Dashboard/ChartAreaInteractive";
 import StatCard from "@/components/Dashboard/StatCard";
 
 
 const DashboardHome = () => {
     return (
         <div className="p-4" >
-            <div className="grid  md:grid-cols-2 xl:grid-cols-4 gap-4">
+            <div className="grid mb-4 md:grid-cols-2 xl:grid-cols-4 gap-4">
                 <StatCard />
                 <StatCard />
                 <StatCard />
                 <StatCard />
             </div>
+
+            <ChartAreaInteractive/>
+            
         </div>
     );
 };
