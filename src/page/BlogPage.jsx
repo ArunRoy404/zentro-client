c6ca10ee141ee1b3b149778dadbcd6163dcb3550
Abdul Al Roman <mdroman45678910@gmail.com>
2025-10-08 12:48:01 +0600

    feat(blog):successfull add blog contaner page

---
+++ src/page/BlogPage.jsx
@@ -0,0 +1,19 @@
+"use client";
+import { RoomCard } from "@/section/blog/BlogCard";
+
+export default function BlogPageHome() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-4 bg-background">
+      <div className="w-full max-w-6xl space-y-8">
+        <RoomCard
+          title="Living Room"
+          description="The living room is the heart of the home—a space designed for comfort, relaxation, & gathering."
+          area="260 Mtr."
+          feature="Bath"
+          color="Brown"
+          imageUrl="https://onepack.casethemes.net/wp-content/uploads/2025/07/img-blending-1-750x580.webp"
+        />
+      </div>
+    </main>
+  );
+}
