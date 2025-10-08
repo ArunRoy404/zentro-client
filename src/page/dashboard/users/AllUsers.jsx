46050530efd97301a8e5149950cd2926802334a8
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-10-08 23:13:33 +0600

    feat: skeleton and alert added

--- src/page/dashboard/users/AllUsers.jsx
+++ src/page/dashboard/users/AllUsers.jsx
@@ -4,12 +4,13 @@ import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import { DataTable } from "@/components/Dashboard/DataTable"
 import usersColumns from "@/components/Dashboard/users/usersColumns"
-import { Loader2 } from "lucide-react"
+import AlertTable from "@/components/Alert/AlertTable"
+import SkeletonTable from "@/components/Skeleton/SkeletonTable"
+
+
 
 const AllUsers = () => {
     const dataFilter = ["name", "email", "address", "role"]
-
-    // Fetch users with TanStack Query
     const {
         data: users = [],
         isLoading,
@@ -24,20 +25,11 @@ const AllUsers = () => {
     })
 
     if (isLoading) {
-        return (
-            <div className="flex justify-center items-center py-20">
-                <Loader2 className="animate-spin size-6 text-muted-foreground" />
-                <span className="ml-2 text-sm text-muted-foreground">Loading users...</span>
-            </div>
-        )
+        return <SkeletonTable />
     }
 
-    if (isError) {
-        return (
-            <div className="text-center text-red-500 py-10">
-                Failed to load users: {error.message}
-            </div>
-        )
+    if (error) {
+        return <AlertTable message={error?.message} label={'User'} />
     }
 
     return (
