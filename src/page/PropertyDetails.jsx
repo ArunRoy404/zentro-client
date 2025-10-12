3669244ec68729e4ed69d0c39200ff1b7ff4d3e8
ArunRoy404 <arunroy0065@gmail.com>
2025-10-12 08:54:33 +0600

    fix: layout fixed

--- src/page/PropertyDetails.jsx
+++ src/page/PropertyDetails.jsx
@@ -21,7 +21,7 @@ export default function PropertyDetails() {
     const fetchProperty = async () => {
       try {
         const res = await fetch(
-          `http://localhost:5000/api/v1/property/get-single-property/${id}`
+          `https://zentro-server.vercel.app/api/v1/property/get-single-property/${id}`
         );
         const data = await res.json();
         if (data.success) setPropertyData(data.data);
@@ -50,40 +50,42 @@ export default function PropertyDetails() {
   const { property, userInfo, agentInfo } = propertyData;
 
   return (
-    <Section>
-      <div className="pt-14 lg:pt-0 grid grid-cols-1 md:grid-cols-2 gap-8">
-        {/* Left: Image */}
-        <div className="relative w-full h-64 sm:h-80 md:h-full overflow-hidden border border-[var(--shad)">
-          <Image
-            src={property.images}
-            alt={property.title}
-            fill
-            className="object-cover"
-          />
+    <main className="pt-16 md:pt-17">
+      <Section>
+        <div className=" lg:pt-0 grid grid-cols-1 md:grid-cols-2 gap-8">
+          {/* Left: Image */}
+          <div className="relative w-full h-64 sm:h-80 md:h-full overflow-hidden border border-[var(--shad)">
+            <Image
+              src={property.images}
+              alt={property.title}
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          {/* Right: Property Info */}
+          <div className="flex flex-col justify-between">
+            <PropertyInfo property={property} />
+            <PropertyFeatures features={property.propertyFeatures} />
+            <PropertyActions />
+          </div>
         </div>
 
-        {/* Right: Property Info */}
-        <div className="flex flex-col justify-between">
-          <PropertyInfo property={property} />
-          <PropertyFeatures features={property.propertyFeatures} />
-          <PropertyActions />
+        <div className="mt-10">
+          <PropertyDescription
+            description={property.description}
+          ></PropertyDescription>
         </div>
-      </div>
 
-      <div className="mt-10">
-        <PropertyDescription
-          description={property.description}
-        ></PropertyDescription>
-      </div>
+        <hr className="my-10 border-[var(--shad)]" />
 
-      <hr className="my-10 border-[var(--shad)]" />
+        {/* Agent & User Info */}
 
-      {/* Agent & User Info */}
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <PropertyUserCard user={userInfo} />
-        <PropertyAgentCard agent={agentInfo} />
-      </div>
-    </Section>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <PropertyUserCard user={userInfo} />
+          <PropertyAgentCard agent={agentInfo} />
+        </div>
+      </Section>
+    </main>
   );
 }
--- src/section/PropertyDetails/PropertyActions.jsx
+++ src/section/PropertyDetails/PropertyActions.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ShoppingCart, CreditCard } from "lucide-react";
+import { ShoppingCart, CreditCard, Heart } from "lucide-react";
 
 export default function PropertyActions() {
   return (
     <div className="flex gap-3 mt-6">
       <button className="flex items-center justify-center gap-2 flex-1 bg-[var(--primary)] text-[var(--background)] py-2 font-semibold hover:opacity-90 transition rounded-none">
-        <ShoppingCart size={18} />
-        Add to Cart
+        <Heart size={18} />
+        Add to Favorite
       </button>
 
       <button className="flex items-center justify-center gap-2 flex-1 border border-[var(--primary)] text-[var(--primary)] hover:bg-[var(--primary)] hover:text-[var(--background)] py-2 font-semibold transition rounded-none">
